10f22f388a688b70b3df20e9b8277ebe97a38144
Antonio Whitbeck <a.whitbeck89@gmail.com>
2024-08-08 01:29:08 +0000

    completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,13 +4,13 @@ import React, { useState } from "react";
 const Home = () => {
 	const [currentLight, setCurrentLight] = useState("green")
 	return (
-		<div className="text-center">
-			<div className="stick"></div>
-			<div className="trafficLight">
-				<div className={currentLight === "red" ? "red light glow" : "red light"} onClick={(e) => { currentLight !== "red" ? setCurrentLight("red") : setCurrentLight("") }}></div>
-				<div className={currentLight === "yellow" ? "yellow light glow" : "yellow light"} onClick={(e) => { currentLight !== "yellow" ? setCurrentLight("yellow") : setCurrentLight("") }}></div>
-				<div className={currentLight === "green" ? "green light glow" : "green light"} onClick={(e) => { currentLight !== "green" ? setCurrentLight("green") : setCurrentLight("") }}></div>
-
+		<div className="wrapper h-100 align-items-center justify-content-center d-flex">
+			<div className="shell">
+				<div className="trafficLight">
+					<div className={currentLight === "red" ? "light lightOn redGlow" : "red light"} onClick={(e) => { currentLight !== "red" ? setCurrentLight("red") : setCurrentLight("") }}></div>
+					<div className={currentLight === "yellow" ? "light lightOn yellowGlow" : "yellow light"} onClick={(e) => { currentLight !== "yellow" ? setCurrentLight("yellow") : setCurrentLight("") }}></div>
+					<div className={currentLight === "green" ? "light lightOn greenGlow" : "green light"} onClick={(e) => { currentLight !== "green" ? setCurrentLight("green") : setCurrentLight("") }}></div>
+				</div>
 			</div>
 		</div>
 	);
